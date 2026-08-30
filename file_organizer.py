5a5ea625d939180b61c8ac0017e0a10e127295a3
Sina <www.mahonia@gmail.com>
2026-08-31 00:26:20 +0330

    Json File Was Created

--- file_organizer.py
+++ file_organizer.py
@@ -1,4 +1,10 @@
+import os
+import json
 from pathlib import Path
+
+if not os.path.exists("Categories.json"):
+    with open("Categories.json" , "w") as file:
+        json.dump({} , file)
 
 class FileOrganizer:
     pass
