d2f3dbc29c9d1f7bcae1e3758b4c793662019577
Sina <www.mahonia@gmail.com>
2026-08-31 12:25:39 +0330

    load file and save file edited and folder_path was created

--- file_organizer.py
+++ file_organizer.py
@@ -2,29 +2,42 @@ import os
 import json
 from pathlib import Path
 
-if not os.path.exists("Categories.json"):
-    with open("Categories.json" , "w") as file:
-        json.dump({} , file)
+
 
 class FileOrganizer:
     def __init__(self):
         self.category = {
             "Images" : [".jpg" , ".png" , ".jpeg"] ,
             "Documents" : [".pdf" , ".docx" , ".txt"] , 
-            "Music" : [".mp3" , ".mav"] ,
+            "Music" : [".mp3" , ".wav"] ,
             "Videos" : [".mp4" , ".mkv"] ,
             "Archives" : [".zip" , ".rar"]
         }
+        
+        if os.path.exists("Categories.json"):
+            self.load_file()
+        else:
+            self.save_file()
 
-        self.save_file()
 
     def save_file(self):
         with open("Categories.json" , "w") as file:
             json.dump(self.category , file , indent=4)
 
+    def load_file(self):
+        with open("Categories.json" , "r") as file:
+            self.category = json.load(file)
+
+    def folder_path(self , path):
+        self.f_path = Path(path)
+        
+        for i in self.f_path.iterdir():
+            for x , y in self.category.items():
+                if i.suffix in y:
+                    print(x)
 
 
 
 acc = FileOrganizer()
 
-print(acc)
+print(acc.folder_path("C:/Users/Sina/Downloads/Programs"))
