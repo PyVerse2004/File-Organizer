e04543598eae9d4ba191ae890d3bb77d1318ac76
Mr.Sina Nosrati <sinatrader2004@gmail.com>
2026-09-01 16:46:15 +0330

    move_file Was created

--- file_organizer.py
+++ file_organizer.py
@@ -34,18 +34,24 @@ class FileOrganizer:
             if i.is_file():
                 for x , y in self.category.items():
                     if i.suffix in y:
-                        dst = self.f_path / x
-                        shutil.move(i , dst)
+                        print(x)
+
 
     def create_folders(self):
         for category, extensions in self.category.items():
             folder = self.f_path / category
             folder.mkdir(exist_ok=True)
 
-
+    def move_file(self):
+           for i in self.f_path.iterdir():
+                if i.is_file():
+                    for x , y in self.category.items():
+                        if i.suffix in y:
+                            dst = self.f_path / x
+                            shutil.move(i , dst)
         
 
 acc = FileOrganizer()
-
-print(acc.folder_path("C:/Users/Sina/Downloads/Documents"))
+acc.folder_path("C:/Users/Sina/Downloads/Documents")
 acc.create_folders()
+acc.move_file()
