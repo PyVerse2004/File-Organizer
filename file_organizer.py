d00ce7d1852293d3d556a1841ac1aeb3604285eb
Sina <www.mahonia@gmail.com>
2026-09-04 14:16:18 +0330

    folder_path was edited and unique and get category and organize function and process method was created

--- file_organizer.py
+++ file_organizer.py
@@ -7,146 +7,91 @@ from pathlib import Path
 class FileOrganizer:
     def __init__(self):
         self.category = {
-            "Images" : [".jpg" , ".png" , ".jpeg"] ,
-            "Documents" : [".pdf" , ".docx" , ".txt"] , 
-            "Music" : [".mp3" , ".wav"] ,
-            "Videos" : [".mp4" , ".mkv"] ,
-            "Archives" : [".zip" , ".rar"],
-            "Other" : []
+            "Images": [".jpg", ".png", ".jpeg"],
+            "Documents": [".pdf", ".docx", ".txt"],
+            "Music": [".mp3", ".wav"],
+            "Videos": [".mp4", ".mkv"],
+            "Archives": [".zip", ".rar"],
+            "Other": []
         }
-        
+
         if os.path.exists("Categories.json"):
             self.load_file()
         else:
             self.save_file()
 
     def save_file(self):
-        with open("Categories.json" , "w") as file:
-            json.dump(self.category , file , indent=4)
+        with open("Categories.json", "w") as file:
+            json.dump(self.category, file, indent=4)
 
     def load_file(self):
-        with open("Categories.json" , "r") as file:
+        with open("Categories.json", "r") as file:
             self.category = json.load(file)
 
-    def folder_path(self , path):
+    def folder_path(self, path):
         self.f_path = Path(path)
-        
-        for i in self.f_path.iterdir():
-            if i.is_file():
-                for x , y in self.category.items():
-                    if i.suffix in y:
-                        print(x)
 
     def create_folders(self):
-        for category, extensions in self.category.items():
+        for category in self.category:
             folder = self.f_path / category
             folder.mkdir(exist_ok=True)
 
+    def get_unique_path(self, folder, file):
+        destination = folder / file.name
+
+        if not destination.exists():
+            return destination
+
+        counter = 1
+
+        while True:
+            new_name = f"{file.stem}_{counter}{file.suffix}"
+            destination = folder / new_name
+
+            if not destination.exists():
+                return destination
+
+            counter += 1
+
+    def get_category(self, file):
+        for category, extensions in self.category.items():
+            if category == "Other":
+                continue
+
+            if file.suffix.lower() in extensions:
+                return category
+
+        return "Other"
+
+    def organize_file(self, file, operation):
+        category = self.get_category(file)
+
+        if category == "Other":
+            if file.suffix.lower() not in self.category["Other"]:
+                self.category["Other"].append(file.suffix.lower())
+                self.save_file()
+
+        destination_folder = self.f_path / category
+        destination = self.get_unique_path(destination_folder, file)
+
+        if operation == "move":
+            shutil.move(file, destination)
+
+        elif operation == "copy":
+            shutil.copy2(file, destination)
+
+    def process_files(self, operation):
+        processed = 0
+
+        for file in self.f_path.iterdir():
+            if file.is_file():
+                self.organize_file(file, operation)
+                processed += 1
+
+        return processed
+
     def move_file(self):
-        for i in self.f_path.iterdir():
-            if i.is_file():
-
-                found = False
-
-                for x, y in self.category.items():
-                    if x == "Other":
-                        continue
-
-                    if i.suffix in y:
-                        dst = self.f_path / x / i.name
-
-                        if dst.exists():
-                            counter = 1
-
-                            while True:
-                                new_name = f"{i.stem}_{counter}{i.suffix}"
-                                new_dst = self.f_path / x / new_name
-
-                                if not new_dst.exists():
-                                    dst = new_dst
-                                    break
-
-                                counter += 1
-
-                        shutil.move(i, dst)
-                        found = True
-                        break
-
-                if not found:
-                    if i.suffix not in self.category["Other"]:
-                        self.category["Other"].append(i.suffix)
-                        self.save_file()
-
-                    dst = self.f_path / "Other" / i.name
-
-                    if dst.exists():
-                        counter = 1
-
-                        while True:
-                            new_name = f"{i.stem}_{counter}{i.suffix}"
-                            new_dst = self.f_path / "Other" / new_name
-
-                            if not new_dst.exists():
-                                dst = new_dst
-                                break
-
-                            counter += 1
-
-                    shutil.move(i, dst)
+        return self.process_files("move")
 
     def copy_file(self):
-        for i in self.f_path.iterdir():
-            if i.is_file():
-
-                found = False
-
-                for x, y in self.category.items():
-                    if x == "Other":
-                        continue
-
-                    if i.suffix in y:
-                        dst = self.f_path / x / i.name
-
-                        if dst.exists():
-                            counter = 1
-
-                            while True:
-                                new_name = f"{i.stem}_{counter}{i.suffix}"
-                                new_dst = self.f_path / x / new_name
-
-                                if not new_dst.exists():
-                                    dst = new_dst
-                                    break
-
-                                counter += 1
-
-                        shutil.copy2(i, dst)
-                        found = True
-                        break
-
-                if not found:
-                    if i.suffix not in self.category["Other"]:
-                        self.category["Other"].append(i.suffix)
-                        self.save_file()
-
-                    dst = self.f_path / "Other" / i.name
-
-                    if dst.exists():
-                        counter = 1
-
-                        while True:
-                            new_name = f"{i.stem}_{counter}{i.suffix}"
-                            new_dst = self.f_path / "Other" / new_name
-
-                            if not new_dst.exists():
-                                dst = new_dst
-                                break
-
-                            counter += 1
-
-                    shutil.copy2(i, dst)
-
-acc = FileOrganizer()
-acc.folder_path("C:/Users/Sina/Downloads/Documents")
-acc.create_folders()
-acc.move_file()
+        return self.process_files("copy")
